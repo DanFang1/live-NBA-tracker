f1777977041f965d87aad45f8485d9d4c4f3d494
Danny Fang <Dannyfang2007@gmail.com>
2026-06-06 23:04:26 -0400

    created a function to get a player's id

--- backend/data/fetch_historical.py
+++ backend/data/fetch_historical.py
@@ -1,3 +1,9 @@
 from nba_api.stats.endpoints import playergamelog
 from nba_api.stats.static import players
 import pandas as pd
+
+def get_player_id(name):
+    matches = players.find_players_by_full_name(name)
+    if not matches:
+        raise ValueError(f"No player found for '{name}'")
+    return matches[0]['id']
